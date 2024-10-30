773c67ab3a7fbd6618a3d963737db742ecb1ed05
raniagh <rghzaiel@ggn-group.com>
2024-10-30 10:47:21 +0100

    room page details

--- app/page.jsx
+++ app/page.jsx
@@ -1,7 +1,16 @@
+import Heading from "@/components/Heading";
+import RoomCard from "@/components/RoomCard";
+import rooms from "@/data/rooms.json";
+
 export default function Home() {
   return (
     <>
-      <h1>Hello</h1>
+      <Heading title='Available Rooms' />
+      {rooms.length > 0 ? (
+        rooms.map((room) => <RoomCard room={room} />)
+      ) : (
+        <h2>No rooms available at the moment</h2>
+      )}
     </>
   );
 }
