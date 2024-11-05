9daf64e1d3ca8993368cdc28fe2bf576fa2542b9
raniagh <rghzaiel@ggn-group.com>
2024-11-05 12:08:35 +0100

    delete room feature

---
+++ app/actions/deleteRoom.js
@@ -0,0 +1,56 @@
+"use server";
+
+import { redirect } from "next/navigation";
+import { createSessionClient } from "../../config/appwrite";
+import { cookies } from "next/headers";
+import { Query } from "node-appwrite";
+
+async function deleteRoom(roomId) {
+  const sessionCookie = await cookies().get("appwrite-session");
+  if (!sessionCookie) {
+    redirect("/login");
+  }
+
+  try {
+    const { account, databases } = await createSessionClient(
+      sessionCookie.value
+    );
+    //Get user's ID
+    const user = await account.get();
+    const userId = user.$id;
+
+    // Fetch users rooms
+    const { documents: rooms } = await databases.listDocuments(
+      process.env.NEXT_PUBLIC_APPWRITE_DATABASE,
+      process.env.NEXT_PUBLIC_APPWRITE_COLLECTION_ROOMS,
+      [Query.equal("user_id", userId)]
+    );
+
+    //Find room to delete
+    const roomToDelete = rooms.find((room) => room.$id === roomId);
+
+    //Delete the room
+    if (roomToDelete) {
+      await databases.deleteDocument(
+        process.env.NEXT_PUBLIC_APPWRITE_DATABASE,
+        process.env.NEXT_PUBLIC_APPWRITE_COLLECTION_ROOMS,
+        roomToDelete.$id
+      );
+
+      return {
+        success: true,
+      };
+    } else {
+      return {
+        error: "Room not found",
+      };
+    }
+  } catch (error) {
+    console.log("Failed to delete room", error);
+    return {
+      error: "Failed to delete room",
+    };
+  }
+}
+
+export default deleteRoom;
--- components/MyRoomCard.jsx
+++ components/MyRoomCard.jsx
@@ -1,6 +1,6 @@
 import Link from "next/link";
-import React from "react";
-import { FaEye, FaTrash } from "react-icons/fa";
+import { FaEye } from "react-icons/fa";
+import DeleteRoomButton from "./DeleteRoomButton";
 
 const MyRoomCard = ({ room }) => {
   return (
@@ -10,16 +10,14 @@ const MyRoomCard = ({ room }) => {
       </div>
       <div className='flex flex-col sm:flex-row w-full sm:w-auto sm:space-x-2 mt-2 sm:mt-0'>
         <Link
-          href={`/rooms/${room.id}`}
+          href={`/rooms/${room.$id}`}
           className='bg-blue-500 text-white px-4 py-2 rounded mb-2 sm:mb-0 w-full sm:w-auto text-center hover:bg-blue-700'
         >
           <FaEye className='inline mr-1' />
           View
         </Link>
 
-        <button className='bg-red-500 text-white px-4 py-2 rounded mb-2 sm:mb-0 w-full sm:w-auto text-center hover:bg-red-700'>
-          <FaTrash className='inline mr-1' /> Delete
-        </button>
+        <DeleteRoomButton roomId={room.$id} />
       </div>
     </div>
   );
