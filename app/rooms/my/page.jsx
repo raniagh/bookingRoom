1133f2e2dfcdb42a5822644a07499a328589a1d0
raniagh <rghzaiel@ggn-group.com>
2024-11-05 11:37:58 +0100

    fetch user rooms

--- app/rooms/my/page.jsx
+++ app/rooms/my/page.jsx
@@ -1,7 +1,20 @@
-import React from "react";
+import Heading from "../../../components/Heading";
+import MyRoomCard from "../../../components/MyRoomCard";
+import getMyRooms from "../../actions/getMyRooms";
 
-const My = () => {
-  return <div>page</div>;
+const MyRoomsPage = async () => {
+  const rooms = await getMyRooms();
+
+  return (
+    <>
+      <Heading title='My Rooms' />
+      {rooms.length > 0 ? (
+        rooms.map((room) => <MyRoomCard room={room} key={room.$id} />)
+      ) : (
+        <p>You have no room listings</p>
+      )}
+    </>
+  );
 };
 
-export default page;
+export default MyRoomsPage;
